77c85917a573b0f450105796c8acc548c9bb145b
Shon-92 <53130164+Shon-92@users.noreply.github.com>
2023-09-22 16:00:53 -0400

    Add files via upload

---
+++ java/WhileLoop.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+import java.util.Scanner;
+import java.util.ArrayList;
+public class WhileLoop {
+    public static void main (String[] args){
+        
+    System.out.println("Hello, input every name you can think of");
+    System.out.println("Once you're done type 'Stop");
+    
+    Scanner input = new Scanner(System.in);
+    ArrayList<String> names = new ArrayList<String>();
+    
+    while(names != "Stop"){
+    
+    }
+    }
+}
---
+++ java/com/mycompany/apt5/APT5.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.apt5;
+
+/**
+ *
+ * @author 2167784
+ */
+public class APT5 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
