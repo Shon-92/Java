ff0f162976ed59d266cc18ad7d76da87c29ea7b9
Shon-92 <53130164+Shon-92@users.noreply.github.com>
2023-10-03 08:52:23 -0400

    Update APT5.java

--- java/com/mycompany/apt5/APT5.java
+++ java/com/mycompany/apt5/APT5.java
@@ -3,7 +3,7 @@
  */
 
 package com.mycompany.apt5;
-
+import java.util.Scanner;
 /**
  *
  * @author 2167784
@@ -11,6 +11,30 @@ package com.mycompany.apt5;
 public class APT5 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+      Scanner input = new Scanner(System.in);
+        
+        System.out.println("Hello, please enter as many names you can think of.");
+        System.out.println("Type 'Stop' when you're done.");
+        
+        
+        
+        int numofnames = 0;
+        
+        while(true){
+        String name = input.nextLine();
+        if (name.equalsIgnoreCase("Stop")){
+            break;
+        }
+        
+        else {
+        numofnames++;
+        }
+      
+        
+        
+    }
+        System.out.println("You entered " +numofnames+ " total names.");
+  }
+
     }
 }
